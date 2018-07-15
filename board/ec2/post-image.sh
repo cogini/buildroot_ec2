76f903dde6402987a38dd58bc45844353e0bd72d
ansible-jake <jake@ip-172-31-19-54.ap-northeast-1.compute.internal>
2018-07-15 08:45:34 +0000

    Use our build scripts

---
+++ board/ec2/post-image.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+BOARD_DIR="$(dirname $0)"
+
+cp -f ${BOARD_DIR}/grub.cfg ${TARGET_DIR}/boot/grub/grub.cfg
+# Copy grub 1st stage to binaries, required for genimage
+cp -f ${HOST_DIR}/lib/grub/i386-pc/boot.img ${BINARIES_DIR}
+
+exit $?
